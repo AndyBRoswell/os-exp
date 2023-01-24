db0bf6870d0b8726d286c9bdd01008560eeccceb
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-01-24 18:36:39 +0800

    /

---
+++ windows/1-introduction/sample-loop-and-print/cpu.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <Windows.h>
+
+#include "../common.h"
+
+int main(int argc, char* argv[], char* envp[]) {
+    Sleep(10000);
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
