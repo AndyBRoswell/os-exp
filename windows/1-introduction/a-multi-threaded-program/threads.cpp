14e7c0c85372f6b796917b0e091af1579f8025e1
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-01-24 21:32:55 +0800

    Update threads.cpp

--- windows/1-introduction/a-multi-threaded-program/threads.cpp
+++ windows/1-introduction/a-multi-threaded-program/threads.cpp
@@ -8,9 +8,7 @@ volatile unsigned long long counter;
 unsigned long long loops;
 
 DWORD WINAPI worker(void* arg) {
-    for (unsigned long long i = 0; i < loops; ++i) {
-        ++counter;
-    }
+    for (unsigned long long i = 0; i < loops; ++i) { ++counter; }
     return EXIT_SUCCESS;
 }
 
@@ -22,12 +20,8 @@ int main(int argc, char* argv[], char* envp[]) {
     loops = std::strtoull(argv[1], reinterpret_cast<char**>(argv[1]) + strlen(argv[1]), 10);
     DWORD thread_id[thread_count];
     HANDLE thread_handle[thread_count];
-    for (size_t i = 0; i < thread_count; ++i) {
-        thread_handle[i] = CreateThread(nullptr, 0, worker, nullptr, 0, &thread_id[i]);
-    }
-    for (size_t i = 0; i < thread_count; ++i) {
-        WaitForSingleObject(thread_handle[i], INFINITE);
-    }
+    for (size_t i = 0; i < thread_count; ++i) { thread_handle[i] = CreateThread(nullptr, 0, worker, nullptr, 0, &thread_id[i]); }
+    for (size_t i = 0; i < thread_count; ++i) { WaitForSingleObject(thread_handle[i], INFINITE); }
     std::cout << "Final value: " << counter << std::endl;
     for (size_t i = 0; i < thread_count; ++i) { CloseHandle(thread_handle[i]); }
     return EXIT_SUCCESS;
