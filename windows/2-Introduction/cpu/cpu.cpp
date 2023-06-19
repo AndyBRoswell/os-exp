4f341591cdef93ad598b214c3f9f826be198603b
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-06-19 15:59:13 +0200

    rearranged dir structure

--- windows/2-Introduction/cpu/cpu.cpp
+++ windows/2-Introduction/cpu/cpu.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[], char* envp[]) {
     if (argc != 2) {
-        std::cerr << "usage\n./sample-loop-and-print <string>" << std::endl;
+        std::cerr << "usage\n./cpu <string>" << std::endl;
         exit(1);
     }
     while (true) {
