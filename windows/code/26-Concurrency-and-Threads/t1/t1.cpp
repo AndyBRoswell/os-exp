391c3e72d9507b0597f70ae982bfbbf0ea72cc9f
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-07-03 18:56:48 +0200

    ch26

--- windows/code/26-Concurrency-and-Threads/t1/t1.cpp
+++ windows/code/26-Concurrency-and-Threads/t1/t1.cpp
@@ -3,8 +3,14 @@
 
 #include <Windows.h>
 
+constexpr size_t addition_count = 1e7;
+
+volatile size_t counter = 0;
+
 DWORD my_thread(LPVOID arg) {
-    std::cout << std::format("{}", reinterpret_cast<const char*>(arg)) << std::endl;
+    std::cout << std::format("{}: begin\n", reinterpret_cast<const char*>(arg)) << std::endl;
+    for (size_t i = 0; i < addition_count; ++i) { ++counter; }
+    std::cout << std::format("{}: done\n", reinterpret_cast<const char*>(arg)) << std::endl;
     return EXIT_SUCCESS;
 }
 
@@ -13,7 +19,8 @@ int main(int argc, char* argv[]) {
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
     HANDLE t[2];
-    std::cout << "main: begin" << std::endl;
+    //std::cout << std::format("main: begin (counter = {})", counter) << std::endl;
+    std::cout << "main: begin (counter = " << counter << ")" << std::endl;
     t[0] = CreateThread(nullptr, 0, my_thread, const_cast<char*>("A"), 0, nullptr);
     t[1] = CreateThread(nullptr, 0, my_thread, const_cast<char*>("B"), 0, nullptr);
     if (t[0] == nullptr || t[1] == nullptr) {
@@ -22,6 +29,7 @@ int main(int argc, char* argv[]) {
     }
     WaitForSingleObject(t[0], INFINITE);
     WaitForSingleObject(t[1], INFINITE);
-    std::cout<< "main: end" << std::endl;
+    //std::cout << std::format("main: done with both (counter = {})", counter) << std::endl;
+    std::cout << "main: done with both (counter = " << counter <<")" << std::endl;
     return EXIT_SUCCESS;
 }
