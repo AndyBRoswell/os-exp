858f27ebb75d699598b4307289833f10695d125b
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-07-03 18:17:14 +0200

    ch26

--- windows/code/26-Concurrency-and-Threads/t0/t0.cpp
+++ windows/code/26-Concurrency-and-Threads/t0/t0.cpp
@@ -1,6 +1,23 @@
+#include <format>
+#include <iostream>
+
 #include <Windows.h>
 
+DWORD my_thread(LPVOID arg) {
+    std::cout << std::format("{}\n", reinterpret_cast<const char*>(arg));
+    return 0;
+}
+
 int main(int argc, char* argv[]) {
-    
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    HANDLE t[2];
+    std::cout << "main: begin" << std::endl;
+    t[0] = CreateThread(nullptr, 0, my_thread, const_cast<char*>("A"), 0, nullptr);
+    t[1] = CreateThread(nullptr, 0, my_thread, const_cast<char*>("B"), 0, nullptr);
+    WaitForSingleObject(t[0], INFINITE);
+    WaitForSingleObject(t[1], INFINITE);
+    std::cout<< "main: end" << std::endl;
     return 0;
 }
