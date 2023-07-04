252f460e535e9427c8e63486de75a95b8d4bfd79
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2023-07-04 17:03:29 +0200

    Update join.cpp

--- windows/code/30-Condition-Variables/join/join.cpp
+++ windows/code/30-Condition-Variables/join/join.cpp
@@ -23,14 +23,8 @@ int main(int argc, char* argv[]) {
     InitializeConditionVariable(&c);
     InitializeCriticalSection(&m);
     std::cout << "parent: begin" << std::endl;
-    const HANDLE const child_thread = CreateThread(
-        nullptr,
-        0,
-        child, // If lambda is used, then you can only pass a capture-less lambda here.
-        nullptr,
-        0,
-        nullptr
-    );
+    // If lambda is used, then you can only pass a capture-less lambda here.
+    const HANDLE const child_thread = CreateThread(nullptr, 0, child, nullptr, 0, nullptr);
     EnterCriticalSection(&m);
     while (done == false) { SleepConditionVariableCS(&c, &m, INFINITE); }
     LeaveCriticalSection(&m);
